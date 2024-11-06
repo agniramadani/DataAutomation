b8413c2df30a4825c6cd8f4b8da9666f73a18375
Agni <agni.ramadani@gmail.com>
2024-11-06 17:15:45 +0100

    Modify config.

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
 DATABASE_URL = "sqlite:///./database.db"
-# SMTP_SERVER = "your_smtp_server"        # e.g., "smtp.gmail.com"
-# SMTP_PORT = your_smtp_port              # e.g., 587 for TLS
-# EMAIL_USER = "your_email@example.com"
-# EMAIL_PASSWORD = "your_email_password"
-# NOTIFY_EMAIL = "notify_email@example.com"
